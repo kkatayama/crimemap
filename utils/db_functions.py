6b8fd9e71e72489f7a73d4d89da8822aa19b1e92
kkatayama <kkatayama@udel.edu>
2022-11-16 20:13:23 -0500

    update log method

--- utils/db_functions.py
+++ utils/db_functions.py
@@ -792,8 +792,6 @@ class ErrorsRestPlugin(object):
         def default_error_handler(res):
             if res.content_type == "application/json":
                 logger.info('\n\n=== actual_response ===\n\n')
-                actual_response = app.fn(*args, **kwargs)
-                logger.debug(actual_response)
                 return res.body
             res.content_type = "application/json"
 
