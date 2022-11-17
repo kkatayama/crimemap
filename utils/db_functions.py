c28b912b5b462728575563ff50f952de05ac5786
kkatayama <kkatayama@udel.edu>
2022-11-17 10:23:05 -0500

    update log method

--- utils/db_functions.py
+++ utils/db_functions.py
@@ -739,7 +739,6 @@ def log_to_logger(fn):
         else:
             logger.info(' === ERROR: actual_response is not dict() === ')
             logger.error(actual_response.__dict__)
-            logger.error(dict(actual_response))
             logger.debug(dict(response))
             try:
                 soup = BeautifulSoup(actual_response, 'html5lib')
